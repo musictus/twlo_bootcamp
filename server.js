ce433aaf3e815e1e3edf00e07962931e7cadce53
Sung Hong <musictus@gmail.com>
2019-04-10 13:43:25 -0700

    html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const http = require('http');
+var path = require("path");
 require('dotenv').config()
 
 // Initialize Express
@@ -12,24 +13,24 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const accountSid = process.env.SID;
 const authToken = process.env.TOKEN;
 
-// const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 
 // Send Message
-// function sendMessage() {
-//     client.messages
-//     .create({
-//         body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//         from: '+19292055493',
-//         to: '+16468317059'
-//     })
-//     .then(message => console.log(message.sid));
-// }
+function sendMessage() {
+    client.messages
+    .create({
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+        from: '+19292055493',
+        to: '+16468317059'
+    })
+    .then(message => console.log(message.sid));
+}
 
 // Default
 app.get("/", (req, res) => {
-  res.end("HOMEPAGE");
+  res.sendFile(path.join(__dirname, "index.html"));;
 });
 
 // Receive Message
