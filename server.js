0582b3d881f2c00c9afc00929b308fd0e27c13fe
Sung Hong <shong@c02xt07vjhd4.twilio.internal>
2019-04-10 08:38:59 -0700

    initial

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require("express");
+require('dotenv').config()
+
+// Initialize Express
+var app = express();
+
+
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = process.env.SID;
+const authToken = process.env.TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
