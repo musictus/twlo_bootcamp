cb0bba712774027f4e3b834da37f1c7b8ddf6beb
Sung Hong <musictus@gmail.com>
2019-04-10 11:57:57 -0700

    changes

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 // Dependencies
-var express = require("express");
+const express = require("express");
+const http = require('http');
 require('dotenv').config()
 
 // Initialize Express
-var app = express();
-
-
-
+const app = express();
 // Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-// DANGER! This is insecure. See http://twil.io/secure
 const accountSid = process.env.SID;
 const authToken = process.env.TOKEN;
 const client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
-     to: '+15558675310'
-   })
-  .then(message => console.log(message.sid));
+
+// Send Message
+function sendMessage() {
+    client.messages
+    .create({
+        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+        from: '+19292055493',
+        to: '+16468317059'
+    })
+    .then(message => console.log(message.sid));
+}
+
+
+// Receive Message
+  app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+  
+    twiml.message('The Robots are coming! Head for the hills!');
+  
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  });
+  
+  http.createServer(app).listen(1337, () => {
+    console.log('Express server listening on port 1337');
+  });
